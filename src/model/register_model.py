b3a76c8d5095c2dc6862a3214582a9d181628e11
Amrith <amrithputtur@gmail.com>
2025-01-31 09:10:22 +0530

    model_name_updated

--- src/model/register_model.py
+++ src/model/register_model.py
@@ -68,7 +68,7 @@ def main():
         model_info_path = 'reports/experiment_info.json'
         model_info = load_model_info(model_info_path)
         
-        model_name = "bow_model"
+        model_name = "bow_model_github_actions"
         register_model(model_name, model_info)
     except Exception as e:
         logger.error('Failed to complete the model registration process: %s', e)
